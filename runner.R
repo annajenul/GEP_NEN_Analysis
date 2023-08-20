2eb45471e5ffb8d22200373eae6aaffeec909afd
annajenul <anna.jenul@gmail.com>
2023-08-20 19:09:15 +0200

    add html files

--- runner.R
+++
@@ -1,4 +0,0 @@
-path <- "C:/Users/Stefan/OneDrive - Norwegian University of Life Sciences/Gasto/"
-source(paste0(path, "src/help_functions.R"))
-rmarkdown::render(paste0(path, 'Experiment_1.Rmd'), 'html_document')
-rmarkdown::render(paste0(path, 'Experiment_2.Rmd'), 'html_document')
